98e38d4a8c3c1faca5ed0a50652426beb6c2f2e3
juliusolaifa <jbolaifa@gmail.com>
2023-09-06 18:46:41 -0500

    model is trained in parallel

--- Application/application.R
+++ Application/application.R
@@ -2,10 +2,10 @@ rm(list=ls())
 library(HeritSeq2)
 
 
-realLife <- function(data, covariate, group, fittingModel="NB", slope=FALSE) {
+realLife <- function(data, covariate, group, fittingModel="NB", slope=FALSE, parallel=1) {
   fitModel <- ifelse(fittingModel == "NB", fitNBmodel, fitCPmodel)
   computeVPCModel <- ifelse(fittingModel == "NB", computeNBVPC, computeCPVPC)
-  fit <- fitModel(data, covariate, slope)
+  fit <- fitModel(data, covariate, slope, parallel)
   vpc <- computeVPCModel(fit, group)
   colnames(vpc) <- "vpc"
   rownames(vpc) <- paste("Gene", 1:nrow(vpc))
@@ -27,3 +27,11 @@ fitcp_noslope0 <- realLife(LXSmRNA, X, 0, fittingModel = "CP")
 fitcp_noslope1 <- realLife(LXSmRNA, X, 1, fittingModel = "CP")
 fitcp_slope0 <- realLife(LXSmRNA, X, 0, fittingModel = "CP", slope = TRUE)
 fitcp_slope1 <- realLife(LXSmRNA, X, 1, fittingModel = "CP", slope = TRUE)
+
+
+
+# res <- microbenchmark(  # Some code
+#   operation1 = fitCPmodel(LXSmRNA[1:10,], X, parallel=4),
+#   operation2 = fitCPmodel(LXSmRNA[1:10,], X, parallel=6),# Some other code
+#   times = 1            # Number of times to run each operation
+# )
